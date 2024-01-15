6ab44334c47a739343eaac9b894600425c48cbd8
Swann <goldenland046@gmail.com>
2024-01-15 15:13:34 +0100

    Create EClasses.py

---
+++ Enums/EClasses.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class EClasses(Enum):
+    """Enum for classes of the game"""
+    Assassin = 1
+    Fighter = 2
+    Healer = 3
+    Mage = 4
+    Ranger = 5
+    Tank = 6
